c24180aa85dfc3505bcc465b7d8016dd43c436e0
Kiattisak Chantharamaneechote <32291012+nutkiattisak@users.noreply.github.com>
2024-08-24 22:03:39 +0700

    fix: update random & add from age, gender

--- src/components/WheelComponent.vue
+++ src/components/WheelComponent.vue
@@ -2,6 +2,25 @@
   <div
     class="flex flex-col items-center justify-center min-h-screen p-4 bg-gradient-to-r from-purple-400 to-pink-500"
   >
+    <div class="mt-4 space-y-4 mb-14">
+      <div>
+        <label for="age" class="block text-white">ระบุอายุ</label>
+        <input
+          id="age"
+          type="number"
+          v-model="age"
+          class="w-full px-4 py-2 mt-2 text-black rounded"
+        />
+      </div>
+      <div class="flex items-center space-x-4">
+        <input type="radio" id="male" value="male" v-model="gender" />
+        <label for="male">ป้อจาย</label>
+
+        <input type="radio" id="female" value="female" v-model="gender" />
+        <label for="female">แม่ญิง</label>
+      </div>
+    </div>
+
     <div class="relative mb-8 w-96 h-96">
       <div
         class="relative w-full h-full border-4 border-white rounded-full shadow-lg"
@@ -15,8 +34,7 @@
           :style="getItemStyle(index)"
         >
           <span
-            class="absolute flex flex-row top-3"
-            :style="getSpanStyle(index)"
+            class="absolute flex flex-row transform rotate-180 -translate-x-1/2 -translate-y-1/2 top-3"
           >
             {{ item.id }}
           </span>
@@ -26,7 +44,6 @@
         class="absolute top-0 z-20 transform -translate-x-1/2 -translate-y-1/2 left-1/2"
       >
         <img src="/down-arrow.png" alt="" />
-        <!-- <ArrowDown class="w-8 h-8 text-white filter drop-shadow-lg" /> -->
       </div>
       <div
         class="absolute z-10 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
@@ -34,16 +51,16 @@
         <button
           @click="spinWheel"
           :disabled="isSpinning"
-          class="flex items-center justify-center w-20 h-20 text-purple-600 bg-white rounded-full hover:bg-purple-100 focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
+          class="flex items-center justify-center w-20 h-20 font-semibold text-black bg-white rounded-full hover:bg-purple-100 focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"
         >
-          <div>หมุน</div>
+          หมุน
         </button>
       </div>
     </div>
+
     <div
       v-if="selectedItem"
       class="mt-4 text-2xl font-bold text-center text-white"
-      aria-live="polite"
     >
       <p>ตกที่{{ selectedItem.name }}</p>
       <p>{{ selectedItem.description }}</p>
@@ -51,17 +68,18 @@
   </div>
 </template>
 
-<script setup>
-import { ref, computed, onMounted, onUnmounted } from "vue"
+<script setup lang="ts">
+import { ref, computed, Ref, watch } from "vue"
 import data from "@/data/data.json"
+import Item from "@/types/Item"
 
-console.log(data)
-
-const items = ref(data)
-const rotation = ref(0)
-const selectedItem = ref(null)
-const isSpinning = ref(false)
-const wheel = ref(null)
+const items: Ref<Item[]> = ref(data)
+const rotation: Ref<number> = ref(-16)
+const selectedItem: Ref<Item | null> = ref(null)
+const isSpinning: Ref<boolean> = ref(false)
+const wheel: Ref<HTMLDivElement | null> = ref(null)
+const age: Ref<number> = ref(1)
+const gender: Ref<string> = ref("male")
 
 const wheelStyle = computed(() => {
   const colors = [
@@ -73,10 +91,10 @@ const wheelStyle = computed(() => {
     "#FF8A65",
     "#A5D6A7",
     "#FFD54F",
-    "#FF6B6B",
-    "#4ECDC4",
-    "#45B7D1",
-    "#F7DC6F",
+    "#FF5370",
+    "#64B5F6",
+    "#4DB6AC",
+    "#9575CD",
   ]
   const gradientStops = colors
     .map((color, index) => {
@@ -89,28 +107,31 @@ const wheelStyle = computed(() => {
   return {
     background: `conic-gradient(from 0deg, ${gradientStops})`,
     transform: `rotate(${rotation.value}deg)`,
-    transition: "transform 3s ease-out",
+    transition: "transform 1s ease-out",
   }
 })
 
-const spinButtonStyle = computed(() => ({
-  transform: `rotate(${isSpinning.value ? "360deg" : "0deg"})`,
-  transition: "transform 3s linear",
-}))
-
-const getItemStyle = (index) => ({
+const getItemStyle = (index: number) => ({
   transform: `rotate(${
     (index * 360) / items.value.length + 360 / items.value.length / 2
   }deg)`,
 })
 
-const getSpanStyle = (index) => ({
-  transform: `translate(-50%, -50%) rotate(180deg)`,
-})
-
 const spinWheel = () => {
-  const randomDegrees = Math.random() * 360
-  const newRotation = rotation.value + 360 + randomDegrees
+  if (age.value === null || age.value <= 0) {
+    return
+  }
+
+  if (!gender.value) {
+    return
+  }
+
+  const target = (age.value - 1) / 12
+  const randomDegrees = target * 360
+  const newRotation =
+    gender.value === "male"
+      ? rotation.value - randomDegrees
+      : rotation.value + randomDegrees
 
   rotation.value = newRotation
   selectedItem.value = null
@@ -122,8 +143,10 @@ const spinWheel = () => {
     )
     selectedItem.value = items.value[index]
     isSpinning.value = false
-  }, 5000)
+  }, 1000)
 }
 
-onUnmounted(() => {})
+watch(gender, (newGender) => {
+  rotation.value = newGender === "female" ? 16 : -16
+})
 </script>
